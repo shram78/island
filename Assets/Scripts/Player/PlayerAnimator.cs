4fffbdb12db98a97fbc4c955e13abe588a4cabde
Shram <ra00275@gmail.coom>
2022-07-02 13:19:06 +0300

    add move stickman, base animations and blend speed, joistick, flat.

---
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class PlayerAnimator : MonoBehaviour
+{
+    [SerializeField] private FloatingJoystick _joystick;
+
+    private const string Speed = "Speed";
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
+        {
+            if (Math.Abs(_joystick.Horizontal) > Math.Abs(_joystick.Vertical))
+                PlayWalkAnimation(Math.Abs(_joystick.Horizontal));
+            else
+                PlayWalkAnimation(Math.Abs(_joystick.Vertical));
+        }
+        else
+            PlayIdleAnimation();
+    }
+
+    private void PlayWalkAnimation(float value)
+    {
+        _animator.SetFloat(Speed, value);
+    }
+
+    private void PlayIdleAnimation()
+    {
+        _animator.SetFloat(Speed, 0);
+    }
+}
